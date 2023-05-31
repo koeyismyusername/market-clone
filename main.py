f143a2e2544a7e9a39136a34083ea0b96ede83b3
koeyismyusername <112312898+koeyismyusername@users.noreply.github.com>
2023-05-31 10:24:13 +0900

    타임스탬프도 db에 입력, GET 메서드로 db 데이터 불러오기

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI,UploadFile,Form
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
@@ -14,16 +16,27 @@ async def create_item(
   title:Annotated[str, Form()], 
   price:Annotated[int, Form()], 
   description:Annotated[str, Form()],
-  place:Annotated[str, Form()]):
+  place:Annotated[str, Form()],
+  insertAt:Annotated[int, Form()]):
   
   image_bytes = await image.read()
   
   cur.execute(f"""
-              INSERT INTO Items(image, title, price, description, place)
-              VALUES ('{image_bytes.hex()}', '{title}', '{price}', '{description}', '{place}')
+              INSERT INTO Items(image, title, price, description, place, insertAt)
+              VALUES ('{image_bytes.hex()}', '{title}', '{price}', '{description}', '{place}', '{insertAt}')
               """)
   con.commit()
   
   return  '200'
+
+@app.get("/items")
+def get_Items():
+  con.row_factory = sqlite3.Row
+  cur = con.cursor()
+  records = cur.execute(f"""
+                        SELECT * FROM Items;
+                        """).fetchall()
+  
+  return JSONResponse(jsonable_encoder(dict(record) for record in records))
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
